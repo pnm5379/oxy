233c66c6ac59923733f4688aa7f6f32e5728287f
pnm5379 <pnm5379@rit.edu>
2015-06-28 21:05:16 -0400

    Updates
    Just some updates.

--- oxycontin_users_tweets.py
+++ oxycontin_users_tweets.py
@@ -6,8 +6,11 @@ tweets_file = file("1year_filtered.json")
 #flagged_users_file = file("Oxycontin_Yes_Histogram.txt")
 flagged_users_file = file("Hand_Flagged_Users.txt")
 
+triangle_users_file = file("Oxycontin_User_Triangle_Thirds.txt")
+
 #oxy_tweets = set()
 flagged_users = set()
+triangle_users = set()
 
 line = flagged_users_file.readline()
 while line != "":
@@ -19,9 +22,23 @@ while line != "":
 		line = flagged_users_file.readline()
 flagged_users_file.close()
 
+line = triangle_users_file.readline()
+while line != "":
+	try:
+		triangle_users.add(int(line))
+		line = triangle_users_file.readline()
+	except ValueError:
+		pass
+		line = triangle_users_file.readline()
+triangle_users_file.close()
+
 #print flagged_users
 
+found_users = set()
+found_tris = set()
+
 file = open("Flagged_Users_Tweets.txt","w")
+tris = open("Triangle_Users_Tweets.txt",'w')
 line = tweets_file.readline()
 while line != "":
 	try:
@@ -37,10 +54,34 @@ while line != "":
 			file.write(";")
 			file.write(str(j["text"].encode('unicode-escape')))
 			file.write("\n")
-			#oxy_tweets.add(j["id"])		
+			#oxy_tweets.add(j["id"])
+			if int(j["user"]["id_str"]) not in found_users:
+				found_users.add(int(j["user"]["id_str"]))
+		if int(j["user"]["id_str"]) in triangle_users:
+			tris.write(str(j["id"]))
+			tris.write(";")
+			tris.write(str(j["user"]["id_str"]))
+			tris.write(";")
+			tris.write(str(j["created_at"])[5:25])
+			tris.write(";")
+			tris.write(str(j["text"].encode('unicode-escape')))
+			tris.write("\n")
+			if int(j["user"]["id_str"]) not in found_tris:
+				found_tris.add(int(j["user"]["id_str"]))		
 	except ValueError:
 		pass
 		file.write("\n")
+		tris.write("\n")
 	line = tweets_file.readline()	
 tweets_file.close()
 file.close()
+tris.close()
+
+print "Number of Oxy User's Twitterings Sought:"
+print len(flagged_users)
+print "Number of Oxy User's Twitterings Found:"
+print len(found_users)
+print "Number of Triangle User's Twitterings Sought:"
+print len(triangle_users)
+print "Number of Triangle User's Twitterings Found:"
+print len(found_tris)
