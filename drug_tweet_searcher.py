41f5e497cdd95ae31e1156334b46f03b17eda46c
pnm5379 <pnm5379@rit.edu>
2015-08-10 23:14:44 -0400

    Geo Updates
    Geotag Stuffs

--- drug_tweet_searcher.py
+++ drug_tweet_searcher.py
@@ -15,6 +15,10 @@ flagged_users = set()
 
 count = 0
 file = open("Flagged_Drug_Tweets.txt","w")
+tweet_geotagss = open("Drug_Geotags.txt",'w')
+tweet_geotagss.write(str("Latitude, Longitude"))
+tweet_geotagss.write(str("\n"))
+count1 = 0
 line = tweets_file.readline()
 while line != "":
 	try:
@@ -33,7 +37,16 @@ while line != "":
 			file.write(str(j["text"].encode('unicode-escape')))
 			file.write("\n")
 			flagged_users.add(str(j["user"]["id_str"]))
-			count = count + 1	
+			count = count + 1
+			try:
+				if j["user"]["geo_enabled"]:
+					tweet_geotagss.write(str(j["geo"]["latitude"]))
+					tweet_geotagss.write(str(", "))
+					tweet_geotagss.write(str(j["geo"]["longitude"]))
+					tweet_geotagss.write(str("\n"))
+					count1 = count1 + 1	
+			except KeyError:
+				pass
 	except ValueError:
 		pass
 		file.write("\n")
@@ -43,6 +56,8 @@ file.close()
 	
 print "Number of Flagged Tweets:"	
 print count
+print "Number of Flagged Tweets with Geotags:"
+print count1
 
 count = 0
 file = open("Flagged_Drug_Users.txt","w")
